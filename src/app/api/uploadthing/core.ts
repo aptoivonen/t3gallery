e56a95d51e31584f33681d98903db8d31f6e7778
Aleksi Toivonen <aptoivonen@gmail.com>
2024-10-23 21:24:58 +0300

    clerk middleware usage

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/only-throw-error */
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
@@ -13,6 +13,10 @@ export const ourFileRouter = {
     .middleware(async ({}) => {
       const user = auth();
       if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      const fullUserData = await clerkClient.users.getUser(user.userId);
+      if (!fullUserData?.privateMetadata?.["can-upload"])
+        throw new UploadThingError("User does not have upload permissions");
 
       const { success } = await ratelimit.limit(user.userId);
       if (!success) throw new UploadThingError("Rate limit exceeded");
